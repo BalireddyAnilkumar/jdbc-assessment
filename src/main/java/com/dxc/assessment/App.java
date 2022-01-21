ef5f39cbcd8e31c1a9d997b49e0082a0000c0a25
BalireddyAnilkumar <anilbalireddy6969@gmail.com>
2022-01-21 11:35:47 +0530

    modified

--- src/main/java/com/dxc/assessment/App.java
+++ src/main/java/com/dxc/assessment/App.java
@@ -1,5 +1,12 @@
 package com.dxc.assessment;
 
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.dxc.assessment.dao.AuthorDao;
+import com.dxc.assessment.modal.Author;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        findAllAuthor();
+    }
+    static void findAllAuthor(){
+        
+        AuthorDao dao = new AuthorDao(); 
+
+            
+        try {
+            List<Author> author= dao.findAll();
+            if (author.size() == 0) {
+                System.out.println("No author found");
+                return;
+            }
+            author.forEach(au -> System.out.println(au));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/dxc/assessment/Author.java
@@ -0,0 +1,5 @@
+package com.dxc.assessment;
+
+public class Author {
+
+}
